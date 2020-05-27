e091fd91132214e07df0f3b951eb26b57d2df879
jiasudemotuohe <masteryongyi@163.com>
2020-05-27 14:48:03 +0800

    update loss

--- loss.py
+++ loss.py
@@ -4,17 +4,17 @@
 
 from tensorflow import keras
 import tensorflow as tf
-from config import GRID_SIZE, BATCH_SIZE, ANCHOR_SIZE, ANCHORS
+from config import GRID_SIZE, BATCH_SIZE, ANCHOR_SIZE, ANCHORS, LOSS_COORD_SCALE, LOSS_NOOBJ_SCALE, LOSS_OBJ_SCALE
 
 
 class YoloLoss(keras.losses.Loss):
 
     def __init__(self):
         super(YoloLoss, self).__init__()
+        self.priors = tf.reshape(ANCHORS, [1, 1, 1, ANCHOR_SIZE, 2])
 
         cell_x = tf.reshape(tf.tile(tf.range(GRID_SIZE), [GRID_SIZE]), shape=(1, GRID_SIZE, GRID_SIZE, 1, 1))
         cell_y = tf.transpose(cell_x, perm=(0, 2, 1, 3, 4))
-
         self.cell_grid = tf.tile(tf.concat([cell_x, cell_y], axis=-1), [BATCH_SIZE, 1, 1, ANCHOR_SIZE, 1])
 
     def call(self, y_true, y_pred):
@@ -22,17 +22,19 @@ class YoloLoss(keras.losses.Loss):
         it means there have box when the confidence of box > 0, confidence = IOU
 
         """
-        object_mask = tf.cast(tf.expand_dims(y_true[..., 0] > 0, axis=-1), dtype=tf.float32) # whether have boxs or not,[confidence, x, y, w, h]
+        self.object_mask = tf.cast(y_true[..., 0], dtype=tf.float32)  # whether have boxs or not,[confidence, x, y, w, h]
+        self.num_object_box = tf.reduce_sum(self.object_mask)
 
-        self.pred_xy = tf.sigmoid(y_pred[..., 1:3]) + tf.cast(self.cell_grid, float)
-        self.pred_wh = tf.exp(y_pred[..., 3:5]) * tf.reshape(ANCHORS, [1, 1, 1, ANCHOR_SIZE, 2])
+        self.pred_xy = tf.sigmoid(y_pred[..., 1:3]) + tf.cast(self.cell_grid, tf.float32)
+        self.pred_wh = tf.exp(y_pred[..., 3:5]) * self.priors
 
-        coord_loss = self.coordinate_loss(y_true[..., 0:5], y_pred[..., 0:5], object_mask)
-        conf_loss = self.confidence_loss(y_true, y_pred, object_mask)
+        coord_loss = self.coordinate_loss(y_true[..., 0:5])
+        conf_loss = self.confidence_loss(y_true, y_pred[..., 0])
+        classs_loss = self.class_loss(y_true, y_pred)
 
-        return coord_loss + conf_loss
+        return coord_loss + conf_loss + classs_loss
 
-    def coordinate_loss(self, true_boxs, pred_boxs, object_mask):
+    def coordinate_loss(self, true_boxs):
         """
         coordinate loss contain the xy loss, wh loss
         First: xy_loss ,sigmoid the predict xy, and then add the cell_grad,
@@ -41,36 +43,44 @@ class YoloLoss(keras.losses.Loss):
         ignore the box coordinate loss
 
         """
-        nb_box_mask = tf.reduce_sum(object_mask)
+        object_mask = tf.expand_dims(self.object_mask, axis=-1)
 
         # center_xy loss
         true_center = true_boxs[..., 1:3]
-        xy_loss = tf.reduce_sum(tf.square(true_center - self.pred_xy) * object_mask) / (nb_box_mask + 1e-6)
+        xy_loss = tf.reduce_sum(tf.square(true_center - self.pred_xy) * object_mask) / (self.num_object_box + 1e-6)
 
-        # weight & height loss
+        #  weight & height loss
         true_wh = true_boxs[..., 3:5]
-        wh_loss = tf.reduce_sum(tf.square(true_wh - self.pred_wh) * object_mask) / (nb_box_mask + 1e-6)
+        wh_loss = tf.reduce_sum(tf.square(true_wh - self.pred_wh) * object_mask) / (self.num_object_box + 1e-6)
 
-        return xy_loss + wh_loss
+        return (xy_loss + wh_loss) * LOSS_COORD_SCALE
 
-    def confidence_loss(self, y_true, y_pred, object_mask):
+    def confidence_loss(self, y_true, pred_conf):
         """
-        # true_conf: = iou between true_box and anchor box, iou(Intersection over union) wrong
+        true_conf: = iou between true_box and anchor box, iou(Intersection over union) wrong
         true_conf = iou between true_box and pred_box, and then multiple the probability with object
         conf_mask
 
         """
-        pred_conf = tf.sigmoid(y_pred[..., 0])  # adjust pred_conf to 0 ~ 1
-        object_mask = tf.squeeze(object_mask, axis=-1)
-        nb_object_mask = tf.reduce_sum(object_mask)
+        pred_conf = tf.sigmoid(pred_conf)  # adjust pred_conf to 0 ~ 1
 
         iou = self.iou(y_true[..., 1:5])  # calculate the IOU between true_box and pred_box
         true_conf = iou * y_true[..., 0]
 
-        obj_conf_loss = tf.reduce_sum(tf.square(true_conf - pred_conf) * object_mask) / (nb_object_mask + 1e-6)
-        noobj_conf_loss = tf.reduce_sum(tf.square(true_conf - pred_conf) * (1-object_mask)) / (nb_object_mask + 1e-6)
+        obj_conf_loss = tf.reduce_sum(tf.square(true_conf - pred_conf) * self.object_mask) / (self.num_object_box + 1e-6)
+        noobj_conf_loss = tf.reduce_sum(tf.square(true_conf - pred_conf) * (1 - self.object_mask)) / \
+                          (self.num_object_box + 1e-6)
 
-        return obj_conf_loss + noobj_conf_loss / 2
+        return obj_conf_loss * LOSS_OBJ_SCALE + noobj_conf_loss * LOSS_NOOBJ_SCALE
+
+    def class_loss(self, y_true, y_pred):
+        y_true_class = y_true[..., 5:]
+        y_pred_class = y_pred[..., 5:]
+
+        grid_loss = tf.nn.softmax_cross_entropy_with_logits(y_true_class, y_pred_class, axis=-1)
+        classs_loss = tf.reduce_sum(grid_loss * self.object_mask) / (self.num_object_box + 1e-6)
+
+        return classs_loss
 
     def iou(self, true_box):
         """
